6ded20bf536ae1fe3e562781d4e824fd35abcfe9
drcxzhou <394044090@qq.com>
2025-03-12 14:17:44 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+# import tensorflow as tf
+import numpy as np
+import random
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# m1=tf.constant(([[3, 3]]))
+# m2 = tf.constant(([[2],[2]]))
+# prod = tf.matmul(m1, m2)
+#
+# sess=tf.Session()
+# result = sess.run(prod)
+# print(result)
+# sess.close()
+# a=np.array([-1,-1])
+# b=a[np.newaxis,:]
+# c=[4]
+# print(np.shape(c)[0])
+# print(a.shape)
+# print(b.shape)
+# print(c.shape)
+
+# with tf.Session() as sess:
+#     # tf.range(start, limit, delta)
+#     # a = tf.range(3, 18, 3)
+#     # print(sess.run(a))
+#     # b = tf.range(1,4)
+#     # print(sess.run(b))
+#     # c = tf.range(5)
+#     # print(sess.run(c))
+#     a_indices = tf.stack([tf.range(tf.shape([3])[0], dtype=tf.int32), [3]], axis=1)
+#
+#     print(sess.run(a_indices))
+# dx = 0
+# dy = 0
+# vx = 100
+# vy = 100
+print(random.uniform(-1, 1))
